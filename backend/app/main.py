d6dfb746cf26d4013f73ab514f4ed58ec2bbcc63
PhilipHousdens <philip.houden.04@gmail.com>
2025-02-20 19:18:40 +0700

    testing backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (for dev only)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+names = ["BamBam", "Ball", "Guide"]
+
 @app.get("/")
 def read_api():
-    return {'Welcome': 'Eric'}
+    return {'names': names}
 
