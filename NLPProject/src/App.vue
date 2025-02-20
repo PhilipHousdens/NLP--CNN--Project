a36e8752a5f355a847b9af0766d2aa94b4ffb259
PhilipHousdens <philip.houden.04@gmail.com>
2025-02-20 18:57:15 +0700

    initial commit

---
+++ NLPProject/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="">
+    <h1 class="text-red-500 text-5xl">
+      HELLO BRO!
+    </h1>
+  </div>
+</template>
+
